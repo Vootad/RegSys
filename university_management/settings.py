d1e9cdbb8d072c00db869a21e3991bc9f62be5b5
Anahita <d.anahita.d@gmail.com>
2025-12-15 23:39:40 +0330

    add Student and Professor classes

--- university_management/settings.py
+++ university_management/settings.py
@@ -49,6 +49,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRender',
+    )
 }
 
 MIDDLEWARE = [
