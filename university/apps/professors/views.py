29b1b801b39a8236b5c874793bce760d5c50c94f
Anahita <d.anahita.d@gmail.com>
2026-01-31 16:54:35 +0330

    professors can see enrolled students for each course

--- university/apps/professors/views.py
+++ university/apps/professors/views.py
@@ -6,3 +6,8 @@ from apps.students.models import Enrollment
 def my_teaching_courses(request):
     courses = Course.objects.filter(professor=request.user)
     return render(request, 'professors/my_courses.html', {'courses': courses})
+
+def enrolled_students(request, course_id):
+    course = get_object_or_404(Course, id=course_id, professor=request.user)
+    enrollments = Enrollment.objects.filter(course=course, term="1402-2").order_by('student__last_name')
+    return render(request, 'professors/students.html', {'enrollments': enrollments, 'course': course})
