f6150d0b44bdf29035350e94d108663ca185a88c
Anahita <d.anahita.d@gmail.com>
2026-01-31 16:38:58 +0330

    add proffesors/urls

---
+++ university/apps/professors/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'professors'
+
+urlpatterns = [
+    path('my-courses/', views.my_teaching_courses, name='my_courses'),
+    path('course/<int:course_id>/students/', views.enrolled_students, name='student_list'),
+
+    path('course/remove-student/<int:enrollment_id>/', views.remove_student_by_professor, name='remove_student'),
+]
