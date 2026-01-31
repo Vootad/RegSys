daf71176e323ce8603e991616be4ffa5bcfb1907
Anahita <d.anahita.d@gmail.com>
2026-01-31 16:31:21 +0330

    add final newline to files

--- university/apps/students/views.py
+++ university/apps/students/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Q
+from apps.courses.models import Course, SystemSetting
+from .models import Enrollment
 
-# Create your views here.
+from django.shortcuts import redirect
+
+
+def student_dashboard(request):
+    return redirect('courses:dashboard')
