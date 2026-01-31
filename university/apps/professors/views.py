e1fb719f15e84f3d2257c3954c369d194b965331
Anahita <d.anahita.d@gmail.com>
2026-01-31 16:51:38 +0330

    professors can see their courses

--- university/apps/professors/views.py
+++ university/apps/professors/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from apps.courses.models import Course
+from apps.students.models import Enrollment
+# professors can see their courses
+#
+def my_teaching_courses(request):
+    courses = Course.objects.filter(professor=request.user)
+    return render(request, 'professors/my_courses.html', {'courses': courses})
