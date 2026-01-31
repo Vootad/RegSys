4d0e5c83df380d869e31e99b98cfe96a35f2230d
Anahita <d.anahita.d@gmail.com>
2026-01-31 16:59:44 +0330

    professors can remove students

--- university/apps/professors/views.py
+++ university/apps/professors/views.py
@@ -11,3 +11,9 @@ def enrolled_students(request, course_id):
     course = get_object_or_404(Course, id=course_id, professor=request.user)
     enrollments = Enrollment.objects.filter(course=course, term="1402-2").order_by('student__last_name')
     return render(request, 'professors/students.html', {'enrollments': enrollments, 'course': course})
+
+def remove_student_by_professor(request, enrollment_id):
+    enrollment = get_object_or_404(Enrollment, id=enrollment_id, course__professor=request.user)
+    course_id = enrollment.course.id
+    enrollment.delete()
+    return redirect('professors:student_list', course_id=course_id)
