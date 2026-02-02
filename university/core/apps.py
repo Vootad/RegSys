dbf38913915f664e770e88cd56f1bff7b56c3a93
unknown <sanieielaheh@gmail.com>
2026-02-02 17:43:23 +0330

    remove core app for refactor

--- university/core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class CoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.courses'  # ← این مسیر باید دقیقاً با مسیر اپ تو هماهنگ باشه
